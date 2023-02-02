0a5b739a41bea796799080f438b0c568a97b91eb
gianbartadougall <giavanniavila@gmail.com>
2023-02-02 14:07:09 +1000

    Resolving last merge issues

--- STM32/Library/Inc/datetime.h
+++ STM32/Library/Inc/datetime.h
@@ -39,6 +39,8 @@ uint8_t dt_time_init(dt_time_t* time, uint8_t second, uint8_t minute, uint8_t ho
 
 uint8_t dt_date_init(dt_date_t* date, uint8_t day, uint8_t month, uint16_t year);
 
+uint8_t dt_is_valid_hour_min_period(char* date);
+
 uint8_t dt_time_is_valid(dt_time_t* time);
 uint8_t dt_time_valid(uint8_t second, uint8_t minute, uint8_t hour);
 
